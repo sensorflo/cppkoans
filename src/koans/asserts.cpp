4bdcba4e1d17d2b05080e2c367173bdf9b1c2d4a
Florian Kaufmann <sensorflo@gmail.com>
2013-08-27 18:47:53 +0200

    added rest of koans of area asserts
    copied the koans almost literally from rubykoans

--- src/koans/asserts.cpp
+++ src/koans/asserts.cpp
@@ -12,3 +12,26 @@ TEST(about_asserts,test_assert_with_message)
 {
   EXPECT_TRUE(false) << "This should be true -- Please fix this";
 }
+
+// To understand reality, you must compare your expectations against actual
+// reality.
+TEST(about_asserts,test_assert_equality)
+{
+  int expected_value = __;
+  int actual_value = 1 + 1;
+  EXPECT_TRUE( expected_value == actual_value);
+}
+
+// Some ways of asserting equality are better than others.
+TEST(about_asserts,test_a_better_way_of_asserting_equality)
+{
+  int expected_value = __;
+  int actual_value = 1 + 1;
+  EXPECT_EQ( expected_value, actual_value);
+}
+
+// Sometimes we will ask you to fill in the values.
+TEST(about_asserts,test_fill_in_values)
+{
+  EXPECT_EQ( __, 1+1);
+}
