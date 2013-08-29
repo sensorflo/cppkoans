0f8e6066bd0caa70678b9160df9e6b81dbbb9a78
sensorflo <sensorflo@gmail.com>
2013-08-29 07:51:11 +0200

    added more _.. 'fill me in' variables

--- src/cppkoans.h
+++ src/cppkoans.h
@@ -1,7 +1,10 @@
 #include "gtest/gtest.h"
 
-static const int __ = 123456; 
-static const double ___ = 123.456; 
-static const char* const ____ = "123456"; 
+static const int            __ = 123456; 
+static const double         ___ = 123.456; 
+static const char* const    ____ = "123456"; 
+static const wchar_t* const _____ = L"123456"; 
+static const char           ______ = '@'; 
+static const wchar_t        _______ = L'@'; 
 
 #define KOAN_AREA(area) TEST(area,DISABLED_dummy) {}
